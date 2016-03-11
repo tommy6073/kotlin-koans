4feaeace4731678ee79856208531cf3cab872be4
Tommy <tommyt6073@gmail.com>
2016-03-11 21:31:48 +0900

    Complete ii-13

--- src/ii_collections/_13_Introduction_.kt
+++ src/ii_collections/_13_Introduction_.kt
@@ -18,8 +18,7 @@ fun example0(list: List<Int>) {
 }
 
 fun Shop.getSetOfCustomers(): Set<Customer> {
-    // Return a set containing all the customers of this shop
-    todoCollectionTask()
-//    return this.customers
+    // Return a set containing a≤ll the customers of this shop
+    return this.customers.toSet()
 }
 
